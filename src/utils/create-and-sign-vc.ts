47e4d5513684a1bf5779af5bcbafc92cb9d880e9
Jack Tsai <jtsai0606@gmail.com>
2023-10-06 11:07:47 +0800

    default keys

--- src/utils/create-and-sign-vc.ts
+++ src/utils/create-and-sign-vc.ts
@@ -12,13 +12,15 @@ export const createVc = async (address: string) => {
 
   const issuerDidWithKeys = await didKey.generateFromPrivateKey(
     privateKeyBufferFromString(
-      process.env.NEXT_PUBLIC_ISSUER_EDDSA_PRIVATE_KEY || ""
+      process.env.NEXT_PUBLIC_ISSUER_EDDSA_PRIVATE_KEY ||
+        "9f49873842a2f3d87e5d043e71c109c806189fa8fb987f2ee6682c465b8d10de20c89749843c44ce5c93c467d8fd28a9c4f6db24a1ad5fc582db7af4072af834"
     )
   );
 
   const holderDidWithKeys = await didKey.generateFromPrivateKey(
     privateKeyBufferFromString(
-      process.env.NEXT_PUBLIC_HOLDER_EDDSA_PRIVATE_KEY || ""
+      process.env.NEXT_PUBLIC_HOLDER_EDDSA_PRIVATE_KEY ||
+        "77d4cb97863d92c15f567d04d5c8313a00657d564f1d9a8b276793c1c588a3fe95f1614c10adc0b60b7bd1d13a39ec012657e2ca2d72f0a319c1c2e7ab1b27fd"
     )
   );
 
